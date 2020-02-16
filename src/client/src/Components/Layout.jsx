f7a178c05a7c1e9a3cafc28e3742a6dd0ed2b5ac
nojaf <florian.verdonck@outlook.com>
2020-02-16 19:57:23 +0100

    Redirect to saved 404 path.

--- src/client/src/Components/Layout.jsx
+++ src/client/src/Components/Layout.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useRoutes } from "hookrouter";
+import React, { useEffect } from "react";
+import { useRoutes, navigate } from "hookrouter";
 import routes from "../Routes";
 import { NotFound } from "../Pages";
 import Navigation from "./Navigation";
@@ -9,6 +9,14 @@ import Loading from "./Loading";
 const Layout = () => {
   const routeResult = useRoutes(routes);
   let { loading } = useAuth0();
+  // Redirect from the 404 page.
+  useEffect(() => {
+    const path = localStorage.getItem("path");
+    if (path) {
+      localStorage.removeItem("path");
+      navigate(path);
+    }
+  }, []);
 
   const showLoading = loading;
 
@@ -16,7 +24,7 @@ const Layout = () => {
     <Loading />
   ) : (
     <main>
-      <Navigation role={'admin'} />
+      <Navigation role={"admin"} />
       {routeResult || <NotFound />}
     </main>
   );
