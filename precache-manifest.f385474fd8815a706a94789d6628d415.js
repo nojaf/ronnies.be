0df22c85dc3885f41b07de7ccd0fd0c02b85a9b7
nojaf <florian.verdonck@outlook.com>
2020-02-22 22:00:31 +0100

    Updates

---
+++ precache-manifest.f385474fd8815a706a94789d6628d415.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78c34d6a0400a1772a9c4678067cab5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b040ab8bcfafbc35d28",
+    "url": "/static/css/2.7efaf445.chunk.css"
+  },
+  {
+    "revision": "2d290606f091c485dd77",
+    "url": "/static/css/main.4564cf71.chunk.css"
+  },
+  {
+    "revision": "2b040ab8bcfafbc35d28",
+    "url": "/static/js/2.855008f9.chunk.js"
+  },
+  {
+    "revision": "47a9ed9d77ff510e72664fa282a6d549",
+    "url": "/static/js/2.855008f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d290606f091c485dd77",
+    "url": "/static/js/main.7bffe68e.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
