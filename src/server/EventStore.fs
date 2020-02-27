b59073a8876ea75ab3d975e5190245ace4182eb7
nojaf <florian.verdonck@outlook.com>
2020-02-27 22:09:59 +0100

    Take event save Azure limit into account.

--- src/server/EventStore.fs
+++ src/server/EventStore.fs
@@ -34,11 +34,30 @@ let private createEvent creator event =
       Data = encodeEvent event
       Metadata = Some(EventMetaData.Encode { Creator = creator }) }
 
+[<Literal>]
+let private BatchLimit = 99
+
+let rec private appendToAzureTableStorage (cosmoEvents: EventWrite<JsonValue> seq)  =
+    task {
+        let moreThanBatchLimit = Seq.length cosmoEvents > BatchLimit
+        let batch =
+            if moreThanBatchLimit
+            then Seq.take BatchLimit cosmoEvents
+            else cosmoEvents
+            |> List.ofSeq
+
+        let! _ = eventStore.AppendEvents EventStream Any batch
+
+        if moreThanBatchLimit then
+            let rest = Seq.skip BatchLimit cosmoEvents
+            return! appendToAzureTableStorage rest
+        else
+            return ()
+    }
+
 let appendEvents userId (events: Event list) =
     let cosmoEvents = List.map (createEvent userId) events
-    task {
-        let! _ = eventStore.AppendEvents EventStream Any cosmoEvents
-        return () }
+    task { do! appendToAzureTableStorage cosmoEvents }
 
 let getEvents() =
     task {
