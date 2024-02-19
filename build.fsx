314a9dc2b8c493151f957a10a53512d2effd2265
nojaf <florian.verdonck@outlook.com>
2024-02-19 18:29:14 +0100

    Update build script.

--- build.fsx
+++ build.fsx
@@ -19,10 +19,23 @@ pipeline "Watch" {
     workingDir __SOURCE_DIRECTORY__
     initialize
     stage "main" {
+        stage "emulator" {
+            workingDir __SOURCE_DIRECTORY__
+            run "firebase emulators:start --project=ronnies-210509"
+        }
+        stage "functions" {
+            workingDir (__SOURCE_DIRECTORY__ </> "Functions")
+            run
+                "dotnet fable ./Functions.fsproj -e .js --watch --fableLib \"@fable-org/fable-library-js\" --noCache --test:MSBuildCracker"
+        }
+        stage "app" {
+            workingDir (__SOURCE_DIRECTORY__ </> "App")
+            run
+                "dotnet fable ./App.fsproj -e .jsx --watch -o ./out --fableLib \"@fable-org/fable-library-js\" --noReflection --exclude \"Nojaf.Fable.React.Plugin\" --test:MSBuildCracker"
+            run "bun run dev"
+            paralle
+        }
         paralle
-        run "docker start esm"
-        run "firebase emulators:start --project=ronnies-210509"
-        run "dotnet fsi ./app/dev-server.fsx watch"
     }
     runIfOnlySpecified true
 }
