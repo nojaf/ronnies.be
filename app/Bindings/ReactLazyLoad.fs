f9c09946cf2f6f5e1b4a8993ce24485c92405540
nojaf <florian.verdonck@outlook.com>
2024-03-09 12:30:45 +0100

    Add height to lazy component.

--- app/Bindings/ReactLazyLoad.fs
+++ app/Bindings/ReactLazyLoad.fs
@@ -10,5 +10,8 @@ type LazyLoadProp =
     [<Emit "offset">]
     static member Offset (value : int) : JSX.Prop = "offset", box value
 
+    [<Emit "height">]
+    static member Height (value : int) : JSX.Prop = "height", box value
+
 [<JSX("default as LazyLoad", "react-lazy-load")>]
 let lazyLoad (props : JSX.Prop seq) (children : JSX.Element seq) : JSX.Element = null
